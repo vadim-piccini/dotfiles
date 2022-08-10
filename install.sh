9d82117b453774e0e0bce1dcafa74aabb44fb626
vadim-piccini <vadim.piccini@sothebys.com>
2022-08-10 22:35:32 +0000

    add oh-my-zsh and syntax highlighting

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# replace dotfiles
+# Install zsh and ohmyzsh
+apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Replace dotfiles
 for f in .*
   do
     if [[ -f $f ]] 
@@ -10,8 +14,11 @@ for f in .*
     fi
   done
 
-# install plugvim (vim plugin manager)
+# Install plugvim (vim plugin manager)
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo 'Installation Complete!'
+# Install zsh syntax highlighting with oh my zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo $'Installation Complete! You should probably run \'sudo reboot\'! '
